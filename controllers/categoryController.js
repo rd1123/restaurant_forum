bf04aab2a9f8a908500409c2b535ae9386dcd8ad
rd <rdding1123@gmail.com>
2020-06-28 16:12:39 +0800

    Add categories edit feature

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -3,17 +3,31 @@ const Category = db.Category
 
 module.exports = {
   getCategories: (req, res) => {
+    let cateName = ''
+    if (req.params.id) {
+      Category.findByPk(req.params.id).then(category => {
+        cateName = category.name
+      })
+    }
+
     Category.findAll({
       raw: true,
       nest: true,
     }).then(categories => {
-      return res.render('admin/categories', { categories: categories })
+      return res.render('admin/categories', { categories: categories, cateName })
     })
   },
 
   postCategory: (req, res) => {
-    console.log('post category')
-    res.redirect('admin/categories')
+    if (!req.body.categoryName) {
+      req.flash('error_msg', "name didn't exist")
+      res.redirect('back')
+    }
+    Category.create({
+      name: req.body.categoryName
+    }).then(category => {
+      return res.redirect('/admin/categories')
+    })
   },
 
   putCategory: (req, res) => {

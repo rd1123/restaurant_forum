d0e19fb0d4a59aced784e3b4722fb2a8c7e94107
rd <rdding1123@gmail.com>
2020-06-28 13:23:16 +0800

    Add category feature

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -7,11 +7,15 @@ const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
 
 const User = db.User
 const Restaurant = db.Restaurant
-
+const Category = db.Category
 
 let adminController = {
   getRestaurants: (req, res) => {
-    return Restaurant.findAll({ raw: true }).then(restaurants => {
+    return Restaurant.findAll({
+      raw: true,
+      nest: true,
+      include: [Category]
+    }).then(restaurants => {
       return res.render('admin/restaurants', {
         restaurants: restaurants
       })
@@ -61,11 +65,12 @@ let adminController = {
   },
 
   getRestaurant: (req, res) => {
-    return Restaurant.findByPk(req.params.id, { raw: true }).then(restaurant => {
-      return res.render('admin/restaurant', {
-        restaurant: restaurant
+    return Restaurant.findByPk(req.params.id, { include: [Category] })
+      .then(restaurant => {
+        return res.render('admin/restaurant', {
+          restaurant: restaurant.toJSON()
+        })
       })
-    })
   },
 
   editRestaurant: (req, res) => {

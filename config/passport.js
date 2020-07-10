c6634b5a0ff83faacdf7aa58d22d2f2f3651e7c2
rd <rdding1123@gmail.com>
2020-07-10 12:50:36 +0800

    Add JSON Web Token

--- config/passport.js
+++ config/passport.js
@@ -19,6 +19,7 @@ passport.use(new LocalStrategy(
   }
 ))
 
+
 passport.serializeUser((user, cb) => {
   cb(null, user.id)
 })
@@ -36,5 +37,30 @@ passport.deserializeUser((id, cb) => {
     return cb(null, user)
   })
 })
+
+
+const jwt = require('jsonwebtoken')
+const passportJWT = require('passport-jwt')
+const ExtractJwt = passportJWT.ExtractJwt
+const JwtStrategy = passportJWT.Strategy
+
+let jwtOptions = {}
+jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken()
+jwtOptions.secretOrKey = process.env.JWT_SECRET
+
+let strategy = new JwtStrategy(jwtOptions, function (jwt_payload, next) {
+  User.findByPk(jwt_payload.id, {
+    include: [
+      { model: Restaurant, as: 'FavoritedRestaurants' },
+      { model: Restaurant, as: 'LikedRestaurants' },
+      { model: User, as: 'Followers' },
+      { model: User, as: 'Followings' }
+    ]
+  }).then(user => {
+    if (!user) return next(null, false)
+    return next(null, user)
+  })
+})
+passport.use(strategy)
 
 module.exports = passport
